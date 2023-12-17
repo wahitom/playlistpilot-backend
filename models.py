7597af0939c83cb65cc559aabcd0f869e3e1ad88
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-12-17 18:13:01 +0300

    Setting up alembic

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Text, VARCHAR, DateTime, Integer, TIMESTAMP
+
+# create base model ie all other models are inheriting frm this one
+Base = declarative_base()
+
+# defining events model
+class Playlist(Base):
+    __tablename__ = "playlists"
+
+    # define columns 
+    id = Column(Integer(), primary_key = True)
+    title = Column(Text())
+    description = Column(VARCHAR)
+    image = Column(VARCHAR)
+    rating = Column(Integer())
+    date_created = Column(TIMESTAMP)
+    
